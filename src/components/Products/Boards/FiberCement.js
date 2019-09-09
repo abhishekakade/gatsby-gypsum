a860948c7325d9fa2f0344259e9892fa44e7938a
Abhishek K <abhishekakade@gmail.com>
2019-09-10 05:15:42 +0530

    fix: responsive tables

--- src/components/Products/Boards/FiberCement.js
+++ src/components/Products/Boards/FiberCement.js
@@ -1,44 +1,59 @@
 import React from "react"
+import "../Products.css"
+// import "../../../../public/bootstrap.min.css"
 
+import { CheckSquare } from "styled-icons/fa-solid"
 const FiberCement = () => {
   return (
-    <article>
-      <h4>Fiber Cement Board</h4>
-      <p>
+    <article className="product-details">
+      <h4 className="product-title">Fiber Cement Board</h4>
+      <p className="product-intro">
         Fiber Cement Boards are made from a mix of cement, cellulose fiber and
         fire resistant fillers on Hatschek machines. The products having
         excellent resistance against fire, termite and moisture makes it ideal
         material for varying applications.
       </p>
-      <h5>Advantages</h5>
-      <ul>
-        <li>Fire and Moisture Resistant</li>
-        <li>Doesn't warp or fade</li>
-        <li>
+      <h5 className="product-subtitle">Advantages</h5>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
+          Fire and Moisture Resistant
+        </li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
+          Doesn't warp or fade
+        </li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
           Withstands ultraviolet rays and is impenetrable by insects and birds
         </li>
-        <li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
           It doesn't dent or bump under direct impact and will not become
           brittle in cold temperatures
         </li>
-        <li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
           Fiber cement boards can be used in historical renovations, where other
           cladding materials are not allowed
         </li>
-        <li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
           Because of their long life, fiber cement boards also cut down on
           repair and maintenance costs
         </li>
-        <li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
           Environment Friendly as Fiber cement boards are made from natural
           cellulose fibers and are completely non asbestos
         </li>
-        <li>
+        <li className="list-group-item">
+          <CheckSquare className="checksquare" size="1rem" />
           This product does not contain asbestos, formaldehydes or harmful
           chemicals
         </li>
       </ul>
-      <h5>Application</h5>
+      <h5 className="product-subtitle">Application</h5>
       <p>
         Fiber Cement Boards are mostly used for Internal & External Wall
         cladding, wall partitions, mezzanine flooring, Roof Underlay, False
@@ -49,7 +64,7 @@ const FiberCement = () => {
         Fiber cement boards are a good alternative for Plywood for better fire
         protection and longer durability
       </p>
-      <h5>BOARD PERFORMANCE:</h5>
+      <h5 className="product-subtitle">Board Performance:</h5>
       <p>
         Thermal Conductivity: SIG Fiber Cement Boards are test for 0.21 W/mK for
         6mm thick board.
@@ -63,8 +78,8 @@ const FiberCement = () => {
         vapor condenses on the surface, the board will retain its strength.
       </p>
 
-      <h5>Product Attributes & Benefits</h5>
-      <table className="table">
+      <h5 className="product-subtitle">Product Attributes & Benefits</h5>
+      <table className="table fc1">
         <thead className="thead-dark">
           <tr>
             <th scope="col">Attributes</th>
@@ -123,8 +138,8 @@ const FiberCement = () => {
         </tbody>
       </table>
 
-      <h5>Board Sizes and Thickness</h5>
-      <table className="table">
+      <h5 className="product-subtitle">Board Sizes and Thickness</h5>
+      <table className="table fc2">
         <thead className="thead-dark">
           <tr>
             <th scope="col">Product</th>
@@ -136,82 +151,41 @@ const FiberCement = () => {
 
         <tbody>
           <tr>
-            <td>
-              <p>Board Square Edge</p>
-            </td>
-            <td>
-              <p>5,6,9,12,16,18,20</p>
-            </td>
-            <td>
-              <p>600,610,1200,1220</p>
-            </td>
-            <td>
-              <p>2400, 2440, 2700, 3000, 3200</p>
-            </td>
+            <td>Board Square Edge</td>
+            <td>5,6,9,12,16,18,20</td>
+            <td>600,610,1200,1220</td>
+            <td>2400, 2440, 2700, 3000, 3200</td>
           </tr>
           <tr>
-            <td>
-              <p>Boards Recess Edge</p>
-            </td>
-            <td>
-              <p>9 &amp; 12</p>
-            </td>
-            <td>
-              <p>1200 &amp; 1220</p>
-            </td>
-            <td>
-              <p>2400, 2440, 2700, 3000, 3200</p>
-            </td>
+            <td>Boards Recess Edge</td>
+            <td>9 &amp; 12</td>
+            <td>1200 &amp; 1220</td>
+            <td>2400, 2440, 2700, 3000, 3200</td>
           </tr>
           <tr>
-            <td>
-              <p>Wood Textures Planks</p>
-            </td>
-            <td>
-              <p>8</p>
-            </td>
-            <td>
-              <p>230</p>
-            </td>
-            <td>
-              <p>2500</p>
-            </td>
+            <td>Wood Textures Planks</td>
+            <td>8</td>
+            <td>230</td>
+            <td>2500</td>
           </tr>
           <tr>
-            <td>
-              <p>Designer Board Internal Cladding</p>
-            </td>
-            <td>
-              <p>6, 8 &amp; 12</p>
-            </td>
-            <td>
-              <p>600, 610, 1200, 1220</p>
-            </td>
-            <td>
-              <p>1200, 2400, 2440</p>
-            </td>
+            <td>Designer Board Internal Cladding</td>
+            <td>6, 8 &amp; 12</td>
+            <td>600, 610, 1200, 1220</td>
+            <td>1200, 2400, 2440</td>
           </tr>
           <tr>
-            <td>
-              <p>Designer Board Ceiling</p>
-            </td>
-            <td>
-              <p>5 &amp; 6</p>
-            </td>
-            <td>
-              <p>600, 610</p>
-            </td>
-            <td>
-              <p>600,610,1200,1220</p>
-              <p>&nbsp;</p>
-            </td>
+            <td>Designer Board Ceiling</td>
+            <td>5 &amp; 6</td>
+            <td>600, 610</td>
+            <td>600,610,1200,1220</td>
           </tr>
         </tbody>
       </table>
 
-      <h5>Properties</h5>
+      <h5 className="product-subtitle">Properties</h5>
 
-      <table className="table">
+      <table className="table fc3">
         <thead className="thead-dark">
           <tr>
             <th scope="col">Properties</th>
@@ -221,140 +195,72 @@ const FiberCement = () => {
 
         <tbody>
           <tr>
-            <td>
-              <p>Density</p>
-            </td>
-            <td>
-              <p>≥ 1300kg/m3</p>
-            </td>
+            <td>Density</td>
+            <td>≥ 1300kg/m3</td>
           </tr>
           <tr>
-            <td>
-              <p>Moisture Content</p>
-            </td>
-            <td>
-              <p>≤ 10%</p>
-            </td>
+            <td>Moisture Content</td>
+            <td>≤ 10%</td>
           </tr>
           <tr>
-            <td>
-              <p>PH Value</p>
-            </td>
-            <td>
-              <p>8-9</p>
-            </td>
+            <td>PH Value</td>
+            <td>8-9</td>
           </tr>
           <tr>
-            <td>
-              <p>Flexural Strength</p>
-            </td>
-            <td>
-              <p>≥ 10 MPA</p>
-            </td>
+            <td>Flexural Strength</td>
+            <td>≥ 10 MPA</td>
           </tr>
           <tr>
-            <td>
-              <p>Impact Strength</p>
-            </td>
-            <td>
-              <p>≥ 7.1 kg/m2 (8mm)</p>
-            </td>
+            <td>Impact Strength</td>
+            <td>≥ 7.1 kg/m2 (8mm)</td>
           </tr>
           <tr>
-            <td>
-              <p>Adhesion Bond Strength</p>
-            </td>
-            <td>
-              <p>≥ 7.9 kg/m2 (10mm)</p>
-            </td>
+            <td>Adhesion Bond Strength</td>
+            <td>≥ 7.9 kg/m2 (10mm)</td>
           </tr>
           <tr>
-            <td>
-              <p>Screw Withdraw Strength</p>
-            </td>
-            <td>
-              <p>0.9 – 1.0 N</p>
-            </td>
+            <td>Screw Withdraw Strength</td>
+            <td>0.9 – 1.0 N</td>
           </tr>
           <tr>
-            <td>
-              <p>Fire Resistance</p>
-            </td>
-            <td>
-              <p>Upto 3 Hours (BS EN 12467)</p>
-            </td>
+            <td>Fire Resistance</td>
+            <td>Upto 3 Hours (BS EN 12467)</td>
           </tr>
           <tr>
-            <td>
-              <p>Length variation due to water absorption</p>
-            </td>
-            <td>
-              <p>0.06%</p>
-            </td>
+            <td>Length variation due to water absorption</td>
+            <td>0.06%</td>
           </tr>
           <tr>
-            <td>
-              <p>Thermal Conductivity</p>
-            </td>
-            <td>
-              <p>≤ 0.21/W.mk</p>
-            </td>
+            <td>Thermal Conductivity</td>
+            <td>≤ 0.21/W.mk</td>
           </tr>
           <tr>
-            <td>
-              <p>Water Absorption “After 48 Hours”</p>
-            </td>
-            <td>
-              <p>≤ 34%</p>
-            </td>
+            <td>Water Absorption "After 48 Hours"</td>
+            <td>≤ 34%</td>
           </tr>
           <tr>
-            <td>
-              <p>Asbestos Content</p>
-            </td>
-            <td>
-              <p>Not present</p>
-            </td>
+            <td>Asbestos Content</td>
+            <td>Not present</td>
           </tr>
           <tr>
-            <td>
-              <p>Formaldehyde Content</p>
-            </td>
-            <td>
-              <p>Not Present</p>
-            </td>
+            <td>Formaldehyde Content</td>
+            <td>Not Present</td>
           </tr>
           <tr>
-            <td>
-              <p>Sound Insulation</p>
-            </td>
-            <td>
-              <p>12mm 53db</p>
-            </td>
+            <td>Sound Insulation</td>
+            <td>12mm 53db</td>
           </tr>
           <tr>
-            <td>
-              <p>Maintenance</p>
-            </td>
-            <td>
-              <p>Low</p>
-            </td>
+            <td>Maintenance</td>
+            <td>Low</td>
           </tr>
           <tr>
-            <td>
-              <p>Workability</p>
-            </td>
-            <td>
-              <p>Easy</p>
-            </td>
+            <td>Workability</td>
+            <td>Easy</td>
           </tr>
           <tr>
-            <td>
-              <p>Service Duration</p>
-            </td>
-            <td>
-              <p>Upto 25 Years</p>
-            </td>
+            <td>Service Duration</td>
+            <td>Upto 25 Years</td>
           </tr>
         </tbody>
       </table>

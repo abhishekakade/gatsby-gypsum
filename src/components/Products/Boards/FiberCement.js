12608349f0970360004ab4fb0fc7997741711a3c
Abhishek K <abhishekakade@gmail.com>
2019-09-09 23:41:34 +0530

    todo: add more products

---
+++ src/components/Products/Boards/FiberCement.js
@@ -0,0 +1,365 @@
+import React from "react"
+
+const FiberCement = () => {
+  return (
+    <article>
+      <h4>Fiber Cement Board</h4>
+      <p>
+        Fiber Cement Boards are made from a mix of cement, cellulose fiber and
+        fire resistant fillers on Hatschek machines. The products having
+        excellent resistance against fire, termite and moisture makes it ideal
+        material for varying applications.
+      </p>
+      <h5>Advantages</h5>
+      <ul>
+        <li>Fire and Moisture Resistant</li>
+        <li>Doesn't warp or fade</li>
+        <li>
+          Withstands ultraviolet rays and is impenetrable by insects and birds
+        </li>
+        <li>
+          It doesn't dent or bump under direct impact and will not become
+          brittle in cold temperatures
+        </li>
+        <li>
+          Fiber cement boards can be used in historical renovations, where other
+          cladding materials are not allowed
+        </li>
+        <li>
+          Because of their long life, fiber cement boards also cut down on
+          repair and maintenance costs
+        </li>
+        <li>
+          Environment Friendly as Fiber cement boards are made from natural
+          cellulose fibers and are completely non asbestos
+        </li>
+        <li>
+          This product does not contain asbestos, formaldehydes or harmful
+          chemicals
+        </li>
+      </ul>
+      <h5>Application</h5>
+      <p>
+        Fiber Cement Boards are mostly used for Internal & External Wall
+        cladding, wall partitions, mezzanine flooring, Roof Underlay, False
+        Ceiling, Office cubical partitions, Fixed Wardrobes, Kitchen cabinets,
+        Duct Covering.
+      </p>
+      <p>
+        Fiber cement boards are a good alternative for Plywood for better fire
+        protection and longer durability
+      </p>
+      <h5>BOARD PERFORMANCE:</h5>
+      <p>
+        Thermal Conductivity: SIG Fiber Cement Boards are test for 0.21 W/mK for
+        6mm thick board.
+      </p>
+      <p>
+        Effect of Temperature: SIG Fiber Cement Boards are not recommended to be
+        stored in temperature above 500 for prolonged period.
+      </p>
+      <p>
+        Effect of condensation: SIG Fiber Cement Boards are vapor permeable. If
+        vapor condenses on the surface, the board will retain its strength.
+      </p>
+
+      <h5>Product Attributes & Benefits</h5>
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">Attributes</th>
+            <th scope="col">Benefits</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Asbestos Free</td>
+            <td>Breathe easy environment</td>
+          </tr>
+          <tr>
+            <td>Moisture Resistant</td>
+            <td>Long lasting</td>
+          </tr>
+          <tr>
+            <td>Sound/Thermal Insulation</td>
+            <td>Comfortable Interior</td>
+          </tr>
+          <tr>
+            <td>Termite Resistance</td>
+            <td>Not Degraded by any living organism</td>
+          </tr>
+          <tr>
+            <td>Dimensional Stability</td>
+            <td>Within Tolerance Limits</td>
+          </tr>
+
+          <tr>
+            <td>Fire Resistant</td>
+            <td>Non-Combustible; Non Toxic</td>
+          </tr>
+
+          <tr>
+            <td>Flexible</td>
+            <td>Ensure Double Handling</td>
+          </tr>
+          <tr>
+            <td>Easy Workability</td>
+            <td>Very easy to use with carpenter tools</td>
+          </tr>
+
+          <tr>
+            <td>Good Adhesion</td>
+            <td>Can Take all type of Finishes (Paint/Wall Paper)</td>
+          </tr>
+          <tr>
+            <td>Eco Friendly</td>
+            <td>Wood Substitute</td>
+          </tr>
+
+          <tr>
+            <td>Weather Resistance</td>
+            <td>Withstands cyclic weather changes</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h5>Board Sizes and Thickness</h5>
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">Product</th>
+            <th scope="col">Thickness mm</th>
+            <th scope="col">Width mm</th>
+            <th scope="col">Length mm</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <p>Board Square Edge</p>
+            </td>
+            <td>
+              <p>5,6,9,12,16,18,20</p>
+            </td>
+            <td>
+              <p>600,610,1200,1220</p>
+            </td>
+            <td>
+              <p>2400, 2440, 2700, 3000, 3200</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Boards Recess Edge</p>
+            </td>
+            <td>
+              <p>9 &amp; 12</p>
+            </td>
+            <td>
+              <p>1200 &amp; 1220</p>
+            </td>
+            <td>
+              <p>2400, 2440, 2700, 3000, 3200</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Wood Textures Planks</p>
+            </td>
+            <td>
+              <p>8</p>
+            </td>
+            <td>
+              <p>230</p>
+            </td>
+            <td>
+              <p>2500</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Designer Board Internal Cladding</p>
+            </td>
+            <td>
+              <p>6, 8 &amp; 12</p>
+            </td>
+            <td>
+              <p>600, 610, 1200, 1220</p>
+            </td>
+            <td>
+              <p>1200, 2400, 2440</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Designer Board Ceiling</p>
+            </td>
+            <td>
+              <p>5 &amp; 6</p>
+            </td>
+            <td>
+              <p>600, 610</p>
+            </td>
+            <td>
+              <p>600,610,1200,1220</p>
+              <p>&nbsp;</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h5>Properties</h5>
+
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">Properties</th>
+            <th scope="col">Specifications</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <p>Density</p>
+            </td>
+            <td>
+              <p>≥ 1300kg/m3</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Moisture Content</p>
+            </td>
+            <td>
+              <p>≤ 10%</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>PH Value</p>
+            </td>
+            <td>
+              <p>8-9</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Flexural Strength</p>
+            </td>
+            <td>
+              <p>≥ 10 MPA</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Impact Strength</p>
+            </td>
+            <td>
+              <p>≥ 7.1 kg/m2 (8mm)</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Adhesion Bond Strength</p>
+            </td>
+            <td>
+              <p>≥ 7.9 kg/m2 (10mm)</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Screw Withdraw Strength</p>
+            </td>
+            <td>
+              <p>0.9 – 1.0 N</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Fire Resistance</p>
+            </td>
+            <td>
+              <p>Upto 3 Hours (BS EN 12467)</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Length variation due to water absorption</p>
+            </td>
+            <td>
+              <p>0.06%</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Thermal Conductivity</p>
+            </td>
+            <td>
+              <p>≤ 0.21/W.mk</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Water Absorption “After 48 Hours”</p>
+            </td>
+            <td>
+              <p>≤ 34%</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Asbestos Content</p>
+            </td>
+            <td>
+              <p>Not present</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Formaldehyde Content</p>
+            </td>
+            <td>
+              <p>Not Present</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Sound Insulation</p>
+            </td>
+            <td>
+              <p>12mm 53db</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Maintenance</p>
+            </td>
+            <td>
+              <p>Low</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Workability</p>
+            </td>
+            <td>
+              <p>Easy</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Service Duration</p>
+            </td>
+            <td>
+              <p>Upto 25 Years</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  )
+}
+
+export default FiberCement
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Products.css"
+import FiberCement from "./Boards/FiberCement"
 const Products = () => {
   return (
     <section id="products">
       <h2>Products</h2>
+      <FiberCement />
     </section>
   )
 }
